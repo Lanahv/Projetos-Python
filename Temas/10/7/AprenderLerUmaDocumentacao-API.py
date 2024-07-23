5da1efe228cde0969a058469d7a6cfac5794878e
Márcio Fernando Maia <59803321+marciomaia2020@users.noreply.github.com>
2024-07-23 09:02:40 -0300

    Criado um novo projeto para adicionar usuários ao sistema
    Insere o usuário mais o seu e-mail tbm... Este projeto será ampliado para adicionar o nome, telefone, email, placa do carro, setor, profissão e o que for nescessário

---
+++ Temas/10/7/AprenderLerUmaDocumentacao-API.py
@@ -0,0 +1,12 @@
+#APIs prontas
+# Utilizando uma API existente
+import requests
+
+resposta = requests.get('https://api.exchangerate-api.com/v4/latest/USD')
+dados = resposta.json()
+print(dados)
+
+
+#Interpretar documentação
+# Interpretação de documentação é essencial para trabalhar com APIs
+# Exemplo: https://requests.readthedocs.io/en/latest/
